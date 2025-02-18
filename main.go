5ad5267d36e08be50998930970462d9857fa61b1
vdeq79 <xszh79@outlook.com>
2025-02-18 17:04:15 +0100

    Add delete button

--- main.go
+++ main.go
@@ -6,16 +6,30 @@ import (
 
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/middleware/cors"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 type Todo struct {
-	Id        int    `json:"id"`
-	Completed bool   `json:"completed"`
-	Body      string `json:"body"`
+	Id        uint   `gorm:"PrimaryKey" json:"id"`
+	Completed bool   `gorm:"default:false" json:"completed"`
+	Body      string `gorm:"not null" json:"body"`
 }
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	dsn := "postgres://postgres:password@db:5432/mydatabase"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	fmt.Println("Connected to database")
+
+	//db.AutoMigrate(&Todo{})
+
+	fmt.Println("Database migrated")
 
 	app := fiber.New()
 
@@ -24,13 +38,6 @@ func main() {
 		AllowHeaders: []string{"Origin", "Content-Type", "Accept"},
 	}))
 
-	todos := []Todo{
-		{Id: 1, Completed: false, Body: "Learn Go"},
-		{Id: 2, Completed: false, Body: "Learn React"},
-		{Id: 3, Completed: false, Body: "Learn Fiber"},
-		{Id: 4, Completed: true, Body: "Learn C#"},
-	}
-
 	// Define a route for the GET method on the root path '/'
 	app.Get("/", func(c fiber.Ctx) error {
 		// Send a string response to the client
@@ -38,8 +45,27 @@ func main() {
 	})
 
 	app.Get("/api/todos", func(c fiber.Ctx) error {
+		var todos []Todo
+		result := db.Find(&todos)
+		if result.Error != nil {
+			log.Fatalf("Error fetching todos: %v", result.Error)
+		}
+
 		// Send a string response to the client
 		return c.Status(200).JSON(fiber.Map{"todos": todos})
+	})
+
+	app.Delete("/api/todos/:id", func(c fiber.Ctx) error {
+		id := c.Params("id")
+		var todo Todo
+		result := db.First(&todo, id)
+		if result.Error != nil {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Todo not found"})
+		}
+
+		db.Delete(&todo)
+
+		return c.Status(200).JSON(fiber.Map{"message": "Todo deleted successfully"})
 	})
 
 	// Start the server on port 8080
--- ui/src/components/TodoItem.tsx
+++ ui/src/components/TodoItem.tsx
@@ -1,5 +1,8 @@
-import { Badge,  Box,  Flex,  Text } from "@chakra-ui/react";
+import { Badge,  Box,  Flex,  Spinner,  Text } from "@chakra-ui/react";
 import { useColorModeValue } from "./ui/color-mode";
+import { BASE_URL } from "../App";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { MdDelete } from "react-icons/md";
 
 export type Todo = {
 	id: number;
@@ -8,9 +11,9 @@ export type Todo = {
 };
 
 const TodoItem = ({ todo }: { todo: Todo }) => {
-	/*const queryClient = useQueryClient();
+	const queryClient = useQueryClient();
 
-	const { mutate: updateTodo, isPending: isUpdating } = useMutation({
+	/*const { mutate: updateTodo, isPending: isUpdating } = useMutation({
 		mutationKey: ["updateTodo"],
 		mutationFn: async () => {
 			if (todo.completed) return alert("Todo is already completed");
@@ -30,13 +33,13 @@ const TodoItem = ({ todo }: { todo: Todo }) => {
 		onSuccess: () => {
 			queryClient.invalidateQueries({ queryKey: ["todos"] });
 		},
-	});
+	});*/
 
 	const { mutate: deleteTodo, isPending: isDeleting } = useMutation({
 		mutationKey: ["deleteTodo"],
 		mutationFn: async () => {
 			try {
-				const res = await fetch(BASE_URL + `/todos/${todo._id}`, {
+				const res = await fetch(BASE_URL + `/todos/${todo.id}`, {
 					method: "DELETE",
 				});
 				const data = await res.json();
@@ -52,45 +55,42 @@ const TodoItem = ({ todo }: { todo: Todo }) => {
 			queryClient.invalidateQueries({ queryKey: ["todos"] });
 		},
 	});
-    */
+    
 
 	return (
         <Box bg={useColorModeValue("gray.400", "gray.700")} px={2} my={3} borderRadius={"0.7rem"}>
 
-                <Flex
-                    flex={1}
-                    alignItems={"center"}
-                    p={2}
-                    justifyContent={"space-between"}
-                >
-
-                    <Text
-                        color={todo.completed ? "green.200" : "yellow.200"}
-                        textDecoration={todo.completed ? "line-through" : "none"}
-                    >
+                <Flex flex={1} p={2}>
+                    <Text color={todo.completed ? "green.200" : "yellow.200"} textDecoration={todo.completed ? "line-through" : "none"}>
                         {todo.body}
                     </Text>
-                    {todo.completed && (
-                        <Badge colorPalette='green'>
-                            Done
-                        </Badge>
-                    )}
-                    {!todo.completed && (
-                        <Badge colorPalette='yellow'>
-                            In Progress
-                        </Badge>
-                    )}
-                </Flex>
-                {/* <Flex gap={2} alignItems={"center"}>
-                    <Box color={"green.500"} cursor={"pointer"} onClick={() => updateTodo()}>
-                        {!isUpdating && <FaCheckCircle size={20} />}
-                        {isUpdating && <Spinner size={"sm"} />}
-                    </Box>
-                    <Box color={"red.500"} cursor={"pointer"} onClick={() => deleteTodo()}>
-                        {!isDeleting && <MdDelete size={25} />}
-                        {isDeleting && <Spinner size={"sm"} />}
-                    </Box>
-                </Flex> */}
+					
+					<Flex ml={"auto"}>
+						<Box marginRight={5}>
+							{todo.completed && (
+								<Badge colorPalette='green'>
+									Done
+								</Badge>
+							)}
+							{!todo.completed && (
+								<Badge colorPalette='yellow'>
+									In Progress
+								</Badge>
+							)}
+						</Box>
+
+						{/* <Box color={"green.500"} cursor={"pointer"} onClick={() => updateTodo()}>
+							{!isUpdating && <FaCheckCircle size={20} />}
+							{isUpdating && <Spinner size={"sm"} />}
+						</Box> */}
+
+						<Box color={"red.500"}  cursor={"pointer"} onClick={() => deleteTodo()}>
+							{!isDeleting && <MdDelete size={25} />}
+							{isDeleting && <Spinner size={"sm"} />}
+						</Box>
+					</Flex>
+
+                </Flex> 
 
         </Box>
 	);
